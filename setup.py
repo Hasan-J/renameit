39ca870e03fbd083e631120f37c446464588fff7
HasanJ <hasan_sg@hotmail.com>
2020-10-22 10:46:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,37 +2,46 @@ import codecs
 from setuptools import setup
 
 
-VERSION = '0.1.0'
+VERSION = "0.1.0"
+
 
 def read_file(filename):
     """
     Read a utf8 encoded text file and return its contents.
     """
-    with codecs.open(filename, 'r', 'utf8') as f:
+    with codecs.open(filename, "r", "utf8") as f:
         return f.read()
 
 
 setup(
-    name='renameit',
-    packages=['renameit'],
+    name="renameit",
+    packages=["renameit"],
     version=VERSION,
-    description='File renaming tool.',
-    long_description=read_file('README.md'),
-    license='MIT',
-    author='Hasan Jawad',
-    author_email='hasan_sg@hotmail.com',
-    url='https://github.com/Hasan-J/renameit',
+    description="File renaming tool.",
+    long_description=read_file("README.md"),
+    license="MIT",
+    author="Hasan Jawad",
+    author_email="hasan_sg@hotmail.com",
+    url="https://github.com/Hasan-J/renameit",
     keywords=[
-        'renameit', 'change', 'files', 'renaming', 'naming files',
-        'refactor', 'rename', 'file names'
+        "renameit",
+        "change",
+        "files",
+        "renaming",
+        "naming files",
+        "refactor",
+        "rename",
+        "file names",
     ],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Natural Language :: English',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Natural Language :: English",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
+    install_requires=["boto3>=1.16.0", "fire>=0.3.1"],
+    entry_points={"console_scripts": "renameit = renameit.cli:fire_cli"},
 )
